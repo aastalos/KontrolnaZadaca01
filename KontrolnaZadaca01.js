ccbb8a233a73ce7ac839de3cdf173b78c1d8d2d3
aastalos <atilaastalos@gmail.com>
2023-03-29 19:25:08 +0200

    Push 4

--- KontrolnaZadaca01.js
+++ KontrolnaZadaca01.js
@@ -1,17 +1,11 @@
 //1 (3 BODA)
 //Napisi funkciju koja ispisuje neparne brojeve od 0 do 100
-function neparni_brojevi(
-    for(int i=0; i<=100; i++){
-        if (i)
-    }
-)
+
 //2 (3 BODA)
 //Napisi funkciju koja koristi switch selekciju za pretvaranje numerickog unosa u godisnje u doba. Za neispravan unos ispisi prikladnu poruku.
 //INPUT: 1; OUTPUT: "Jesen";
 //INPUT: 3; OUTPUT: "Ljeto"; 
-function Godisnje_doba(
-
-)
+function 
 
 //3 (4 BODA)
 //Napisi funkciju koja okrece uneseni string i broji charactere uzimajuci u obzir prazna mjesta (eng. whitespaces)
